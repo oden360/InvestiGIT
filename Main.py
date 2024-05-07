e64c0da3ba462b4b437880b7ae4c9d13b8d991a3
odn <[email protected]>
2024-05-07 19:51:35 +0200

    start

---
+++ Main.py
@@ -0,0 +1,12 @@
+import Collector
+import Tools.Curator as Curator
+
+
+
+
+def main(): 
+    composite = Composite()
+    composite.data = CustomData.from_dict(config_data)
+
+if __name__ == "main":
+    main()
